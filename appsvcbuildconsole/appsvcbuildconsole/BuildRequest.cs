f33dca811780a0118c120be3d53117e56f7af363
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-05-18 09:51:17 -0700

    updates

---
+++ appsvcbuildconsole/appsvcbuildconsole/BuildRequest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace appsvcbuildconsole
+{
+    public class BuildRequest
+    {
+        [JsonProperty("stack")]
+        public string Stack;
+
+        [JsonProperty("version")]
+        public string Version;
+
+        [JsonProperty("templateRepoURL")]
+        public string TemplateRepoURL;
+
+        [JsonProperty("templateRepoName")]
+        public string TemplateRepoName;
+
+        [JsonProperty("templateName")]
+        public string TemplateName;
+
+        [JsonProperty("branch")]
+        public string Branch;
+
+        [JsonProperty("baseImage")]
+        public string BaseImage;
+
+        [JsonProperty("outputRepoURL")]
+        public string OutputRepoURL;
+
+        [JsonProperty("outputRepoName")]
+        public string OutputRepoName;
+
+        [JsonProperty("outputImageName")]
+        public string OutputImage;
+
+        [JsonProperty("testWebAppName")]
+        public string TestWebAppName;
+
+        [JsonProperty("email")]
+        public string Email;
+    }
+}
--- appsvcbuildconsole/appsvcbuildconsole/Program.cs
+++ appsvcbuildconsole/appsvcbuildconsole/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace appsvcbuildconsole
@@ -17,82 +20,25 @@ namespace appsvcbuildconsole
         }
         static async void buildAsync()
         {
-            String[] tags =
-            {
-                //"oryxprod/node-4.4:20190401.5",
-                //"oryxprod/node-4.5:20190401.5",
-                //"oryxprod/node-4.8:20190401.5",
-                //"oryxprod/node-6.2:20190401.5",
-                //"oryxprod/node-6.6:20190401.5",
-                //"oryxprod/node-6.9:20190401.5",
-                //"node-6.10:20190401.5",
-                //"node-6.11:20190401.5",
-                //"node-8.0:20190401.5",
-                //"node-8.1:20190401.5",
-                //"node-8.2:20190401.5",
-                //"node-8.8:20190401.5",
-                //"node-8.9:20190401.5"`
-                //"node-8.11:20190401.5",
-                //"oryxprod/node-8.12:20190401.5",
-                //"oryxprod/node-9.4:20190401.5",
-                //"oryxprod/node-10.1:20190401.5",
-                //"oryxprod/node-10.10:20190427.2",
-                //"oryxprod/node-10.12:20190401.5",
-                //"oryxprod/node-10.14:20190427.2"
-                "oryxprod/php-7.3:20190501.3",
-                "oryxprod/php-7.2:20190501.3",
-                "oryxprod/php-7.0:20190501.3",
-                "oryxprod/php-5.6:20190501.3"
-                /*"oryxprod/php-5.6:20190422.4 ",
-                "oryxprod/php-7.0:20190422.4 ",
-                "oryxprod/php-7.2:20190422.4 ",
-                "oryxprod/php-7.3:20190422.4 "*/
-                /*"ruby:2.6.2",
-                "ruby:2.5.5",
-                "ruby:2.4.5",
-                "ruby:2.3.8"*/
-                /*"oryxprod/dotnetcore-1.0:20190301.2",
-                "oryxprod/dotnetcore-1.1:20190301.2",
-                "oryxprod/dotnetcore-2.1:20190301.2",
-                "oryxprod/dotnetcore-2.2:20190422.4"*/
-                /*"oryxprod/python-2.7:20190223.1",
-                "oryxprod/python-3.6:20190223.1,",
-                "oryxprod/python-3.7:20190223.1"*/
-            };
+            String text = File.ReadAllText("../../../requests.json");
+            List<BuildRequest> buildRequests = JsonConvert.DeserializeObject<List<BuildRequest>>(text);
 
-            foreach(String t in tags)
+            foreach (BuildRequest br in buildRequests)
             {
-                await makeRequestAsync(t);
+                await makeRequestAsync(br);
                 System.Threading.Thread.Sleep(1 * 60 * 1000); // sleep 1 mins between builds
             }
         }
 
-        static async Task makeRequestAsync(String tag)
+        static async Task makeRequestAsync(BuildRequest br)
         {
-            Console.WriteLine(String.Format("making tag: {0}", tag));
-            String stack = "";
-            if (tag.ToLower().Contains("python"))
-            {
-                stack = "Python";
-            } else if (tag.ToLower().Contains("node"))
-            {
-                stack = "Node";
-            }
-            else if (tag.ToLower().Contains("ruby"))
-            {
-                stack = "Ruby";
-            } else if (tag.ToLower().Contains("php"))
-            {
-                stack = "Php";
-            } else if (tag.ToLower().Contains("dotnetcore"))
-            {
-                stack = "Dotnetcore";
-            }
+            Console.WriteLine(String.Format("making tag: {0} {1}", br.Stack, br.Version));
+            String stack = String.Format("{0}{1}", br.Stack.ToUpper()[0], br.Stack.ToLower().Substring(1));
             String secretKey = "";
             //String url = String.Format("https://appsvcbuildfunc.azurewebsites.net/api/Http{0}Pipeline?code={1}", stack, secretKey);
             String url = String.Format("http://localhost:7071/api/Http{0}Pipeline", stack);
 
-            String body = String.Format("{{\"newTags\": [\"{0}\"]}}", tag);
+            String body = JsonConvert.SerializeObject(new List<BuildRequest>{ br });
             var client = new RestClient(url);
             client.Timeout = 1000 * 60 * 60; //1h
             var request = new RestRequest(Method.POST);
