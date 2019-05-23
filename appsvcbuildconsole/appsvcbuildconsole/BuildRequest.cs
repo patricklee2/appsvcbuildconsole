107db3a4e93ff38839da9699be64d043eea886b0
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-05-23 16:25:23 -0700

    support durable functions

--- appsvcbuildconsole/appsvcbuildconsole/BuildRequest.cs
+++ appsvcbuildconsole/appsvcbuildconsole/BuildRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace appsvcbuildconsole
@@ -14,17 +15,20 @@ namespace appsvcbuildconsole
         [JsonProperty("templateRepoURL")]
         public string TemplateRepoURL;
 
+        [JsonProperty("templateRepoOrgName")]
+        public string TemplateRepoOrgName;
+
         [JsonProperty("templateRepoName")]
         public string TemplateRepoName;
 
         [JsonProperty("templateName")]
         public string TemplateName;
 
-        [JsonProperty("branch")]
-        public string Branch;
+        [JsonProperty("templateRepoBranchName")]
+        public string TemplateRepoBranchName;
 
-        [JsonProperty("baseImage")]
-        public string BaseImage;
+        [JsonProperty("baseImageName")]
+        public string BaseImageName;
 
         [JsonProperty("outputRepoURL")]
         public string OutputRepoURL;
@@ -32,13 +36,306 @@ namespace appsvcbuildconsole
         [JsonProperty("outputRepoName")]
         public string OutputRepoName;
 
+        [JsonProperty("outputRepoOrgName")]
+        public string OutputRepoOrgName;
+
+        [JsonProperty("outputRepoBranchName")]
+        public string OutputRepoBranchName;
+
         [JsonProperty("outputImageName")]
-        public string OutputImage;
+        public string OutputImageName;
+
+        [JsonProperty("webAppName")]
+        public string WebAppName;
+
+        [JsonProperty("rubyBaseOutputRepoURL")]
+        public string RubyBaseOutputRepoURL;
+
+        [JsonProperty("rubyBaseOutputRepoName")]
+        public string RubyBaseOutputRepoName;
+
+        [JsonProperty("rubyBaseOutputRepoOrgName")]
+        public string RubyBaseOutputRepoOrgName;
+
+        [JsonProperty("rubyBaseOutputRepoBranchName")]
+        public string RubyBaseOutputRepoBranchName;
+
+        [JsonProperty("rubyBaseOutputImageName")]
+        public string RubyBaseOutputImageName;
+
+        [JsonProperty("email")]
+        public string Email;
+
+        [JsonProperty("testTemplateRepoURL")]
+        public string TestTemplateRepoURL;
+
+        [JsonProperty("testTemplateRepoOrgName")]
+        public string TestTemplateRepoOrgName;
+
+        [JsonProperty("testTemplateRepoName")]
+        public string TestTemplateRepoName;
+
+        [JsonProperty("tesTemplateName")]
+        public string TestTemplateName;
+
+        [JsonProperty("testTemplateRepoBranchName")]
+        public string TestTemplateRepoBranchName;
+
+        [JsonProperty("testBaseImageName")]
+        public string TestBaseImageName;
+
+        [JsonProperty("testOutputRepoURL")]
+        public string TestOutputRepoURL;
+
+        [JsonProperty("testOutputRepoName")]
+        public string TestOutputRepoName;
+
+        [JsonProperty("testOutputRepoOrgName")]
+        public string TestOutputRepoOrgName;
+
+        [JsonProperty("testOutputRepoBranchName")]
+        public string TestOutputRepoBranchName;
+
+        [JsonProperty("testOutputImageName")]
+        public string TestOutputImageName;
 
         [JsonProperty("testWebAppName")]
         public string TestWebAppName;
 
-        [JsonProperty("email")]
-        public string Email;
+        private static String getDotnetcoreTemplate(String version)
+        {
+            if (version.StartsWith("1"))
+            {
+                return "debian-8";
+            }
+            else
+            {
+                return "debian-9";
+            }
+        }
+
+        private static String getNodeTemplate(String version)
+        {
+            return "debian-9";
+        }
+
+        private static String getPhpTemplate(String version)
+        {
+
+            if (new List<String> { "5.6", "7.0", "7.2", "7.3" }.Contains(version))
+            {
+                return String.Format("template-{0}-apache", version);
+            }
+
+            throw new Exception(String.Format("unexpected php version: {0}", version));
+        }
+
+        private static String getPythonTemplate(String version)
+        {
+            if (new List<String> { "2.7", "3.6", "3.7" }.Contains(version))
+            {
+                return String.Format("template-{0}", version);
+            }
+
+            throw new Exception(String.Format("unexpected python version: {0}", version));
+        }
+
+        private static String getRubyTemplate(String version)
+        {
+            return "templates";
+        }
+
+        private static String getTemplate(String stack, String version)
+        {
+            if (stack == "dotnetcore")
+            {
+                return getDotnetcoreTemplate(version);
+            }
+            if (stack == "node")
+            {
+                return getNodeTemplate(version);
+            }
+            if (stack == "php")
+            {
+                return getPhpTemplate(version);
+            }
+            if (stack == "python")
+            {
+                return getPythonTemplate(version);
+            }
+            if (stack == "ruby")
+            {
+                return getRubyTemplate(version);
+            }
+
+            throw new Exception(String.Format("unexpected stack: {0}", stack));
+        }
+
+        private static String getTestTemplate(String stack, String version)
+        {
+            if (stack == "dotnetcore")
+            {
+                return "TestAppTemplate";
+            }
+            if (stack == "node")
+            {
+                return "nodeAppTemplate";
+            }
+            if (stack == "php")
+            {
+                return "template-app-apache";
+            }
+            if (stack == "python")
+            {
+                return "template-app";
+            }
+            if (stack == "ruby")
+            {
+                return "TestAppTemplate";
+            }
+
+            throw new Exception(String.Format("unexpected stack: {0}", stack));
+        }
+
+        public void processAddDefaults()
+        {
+            if (Stack == null)
+            {
+                throw new Exception("missing stack");
+            }
+            Stack = Stack.ToLower();
+            if (Version == null)
+            {
+                throw new Exception("missing version");
+            }
+            if (TemplateRepoURL == null)
+            {
+                TemplateRepoURL = String.Format("https://github.com/Azure-App-Service/{0}-template.git", Stack);
+            }
+            if (TemplateRepoName == null)
+            {
+                String[] splitted = TemplateRepoURL.Split('/');
+                TemplateRepoName = splitted[splitted.Length - 1].Replace(".git", "");
+            }
+            if (TemplateRepoOrgName == null)
+            {
+                String[] splitted = TemplateRepoURL.Split('/');
+                TemplateRepoOrgName = splitted[splitted.Length - 2];
+            }
+            if (TemplateName == null)
+            {
+                TemplateName = getTemplate(Stack, Version);
+            }
+            if (TemplateRepoBranchName == null)
+            {
+                TemplateRepoBranchName = "master";
+            }
+            if (BaseImageName == null)
+            {
+                BaseImageName = String.Format("mcr.microsoft.com/oryx/{0}-{1}:latest", Stack, Version);
+            }
+            if (OutputRepoURL == null)
+            {
+                OutputRepoURL = String.Format("https://github.com/blessedimagepipeline/{0}-{1}.git", Stack, Version);
+            }
+            if (OutputRepoName == null)
+            {
+                String[] splitted = OutputRepoURL.Split('/');
+                OutputRepoName = splitted[splitted.Length - 1].Replace(".git", "");
+            }
+            if (OutputRepoOrgName == null)
+            {
+                String[] splitted = OutputRepoURL.Split('/');
+                OutputRepoOrgName = splitted[splitted.Length - 2];
+            }
+            if (OutputRepoBranchName == null)
+            {
+                OutputRepoBranchName = "master";
+            }
+            if (OutputImageName == null)
+            {
+                OutputImageName = String.Format("{0}:{1}", Stack, Version);
+            }
+            if (WebAppName == null)
+            {
+                WebAppName = String.Format("appsvcbuild-{0}-hostingstart-{1}-site", Stack, Version.Replace(".", "-"));
+            }
+            if (Email == null)
+            {
+                Email = "patle@microsoft.com";
+            }
+            if (TestTemplateRepoURL == null)
+            {
+                TestTemplateRepoURL = TemplateRepoURL;
+            }
+            if (TestTemplateRepoName == null)
+            {
+                TestTemplateRepoName = TemplateRepoName;
+            }
+            if (TestTemplateRepoOrgName == null)
+            {
+                TestTemplateRepoOrgName = TemplateRepoOrgName;
+            }
+            if (TestTemplateName == null)
+            {
+                TestTemplateName = getTestTemplate(Stack, Version);
+            }
+            if (TestTemplateRepoBranchName == null)
+            {
+                TestTemplateRepoBranchName = TemplateRepoBranchName;
+            }
+            if (TestBaseImageName == null)
+            {
+                TestBaseImageName = OutputImageName;
+            }
+            if (TestOutputRepoURL == null)
+            {
+                TestOutputRepoURL = String.Format("https://github.com/blessedimagepipeline/{0}-app-{1}.git", Stack, Version);
+            }
+            if (TestOutputRepoName == null)
+            {
+                String[] splitted = TestOutputRepoURL.Split('/');
+                TestOutputRepoName = splitted[splitted.Length - 1].Replace(".git", "");
+            }
+            if (TestOutputRepoOrgName == null)
+            {
+                String[] splitted = TestOutputRepoURL.Split('/');
+                TestOutputRepoOrgName = splitted[splitted.Length - 2];
+            }
+            if (TestOutputRepoBranchName == null)
+            {
+                TestOutputRepoBranchName = "master";
+            }
+            if (TestOutputImageName == null)
+            {
+                TestOutputImageName = String.Format("{0}app:{1}", Stack, Version);
+            }
+            if (TestWebAppName == null)
+            {
+                TestWebAppName = String.Format("appsvcbuild-{0}-app-{1}-site", Stack, Version.Replace(".", "-"));
+            }
+            if (RubyBaseOutputRepoURL == null)
+            {
+                RubyBaseOutputRepoURL = String.Format("https://github.com/blessedimagepipeline/rubybase-{0}.git", Version);
+            }
+            if (RubyBaseOutputRepoName == null)
+            {
+                String[] splitted = RubyBaseOutputRepoURL.Split('/');
+                RubyBaseOutputRepoName = splitted[splitted.Length - 1].Replace(".git", "");
+            }
+            if (RubyBaseOutputRepoOrgName == null)
+            {
+                String[] splitted = RubyBaseOutputRepoURL.Split('/');
+                RubyBaseOutputRepoOrgName = splitted[splitted.Length - 2];
+            }
+            if (RubyBaseOutputRepoBranchName == null)
+            {
+                RubyBaseOutputRepoBranchName = "master";
+            }
+            if (RubyBaseOutputImageName == null)
+            {
+                RubyBaseOutputImageName = String.Format("rubybase:{0}", Version);
+            }
+        }
     }
 }
--- appsvcbuildconsole/appsvcbuildconsole/Program.cs
+++ appsvcbuildconsole/appsvcbuildconsole/Program.cs
@@ -20,9 +20,27 @@ namespace appsvcbuildconsole
         }
         static async void buildAsync()
         {
-            String text = File.ReadAllText("../../../requests.json");
-            List<BuildRequest> buildRequests = JsonConvert.DeserializeObject<List<BuildRequest>>(text);
+            String text = "";
+            List<BuildRequest> buildRequests = new List<BuildRequest>();
 
+            text = File.ReadAllText("../../../kudu.json");
+            //buildRequests.AddRange(JsonConvert.DeserializeObject<List<BuildRequest>>(text));
+
+            text = File.ReadAllText("../../../dotnetcore.json");
+            //buildRequests.AddRange(JsonConvert.DeserializeObject<List<BuildRequest>>(text));
+
+            text = File.ReadAllText("../../../node.json");
+            //buildRequests.AddRange(JsonConvert.DeserializeObject<List<BuildRequest>>(text));
+
+            text = File.ReadAllText("../../../php.json");
+            //buildRequests.AddRange(JsonConvert.DeserializeObject<List<BuildRequest>>(text));
+
+            text = File.ReadAllText("../../../python.json");
+            //buildRequests.AddRange(JsonConvert.DeserializeObject<List<BuildRequest>>(text));
+
+            text = File.ReadAllText("../../../ruby.json");
+            buildRequests.AddRange(JsonConvert.DeserializeObject<List<BuildRequest>>(text));
+            
             foreach (BuildRequest br in buildRequests)
             {
                 await makeRequestAsync(br);
@@ -36,20 +54,65 @@ namespace appsvcbuildconsole
             String stack = String.Format("{0}{1}", br.Stack.ToUpper()[0], br.Stack.ToLower().Substring(1));
             String secretKey = "";
             //String url = String.Format("https://appsvcbuildfunc.azurewebsites.net/api/Http{0}Pipeline?code={1}", stack, secretKey);
-            String url = String.Format("http://localhost:7071/api/Http{0}Pipeline", stack);
+            //String url = String.Format("http://localhost:7071/api/Http{0}Pipeline", stack);
+            //String url = String.Format("https://appsvcbuildfunc-test.azurewebsites.net/api/HttpBuildPipeline_HttpStart?code={0}", secretKey);
+            String url = "http://localhost:7071/api/HttpBuildPipeline_HttpStart";
 
-            String body = JsonConvert.SerializeObject(new List<BuildRequest>{ br });
+            String body = JsonConvert.SerializeObject(br);
             var client = new RestClient(url);
-            client.Timeout = 1000 * 60 * 60; //1h
+            client.Timeout = 1000 *60; // 1min
             var request = new RestRequest(Method.POST);
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("Content-Type", "application/json");
             request.AddParameter("undefined", body, ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
             Console.WriteLine(response.StatusCode.ToString());
-            
-            String result = response.Content.ToString();
-            Console.WriteLine(result);
+            //Console.WriteLine(response.Content.ToString());
+
+            var result = JsonConvert.DeserializeObject<dynamic>(response.Content.ToString());
+            String statusURL = result.statusQueryGetUri;
+            Console.WriteLine(String.Format("{0} {1} statusQueryGetUri {2}", br.Stack, br.Version, statusURL));
+            while (true)
+            {
+                client = new RestClient(statusURL);
+                client.Timeout = 1000 * 60; // 1min
+                request = new RestRequest(Method.GET);
+                request.AddHeader("cache-control", "no-cache");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddParameter("undefined", body, ParameterType.RequestBody);
+                response = client.Execute(request);
+                //Console.WriteLine(response.StatusCode.ToString());
+                //Console.WriteLine(response.Content.ToString());
+                result = JsonConvert.DeserializeObject<dynamic>(response.Content.ToString());
+                String status = result.runtimeStatus;
+                if (!status.ToLower().Equals("running"))
+                {
+                    Console.WriteLine(String.Format("{0} {1} completed", br.Stack, br.Version));
+                    var output = JsonConvert.DeserializeObject<dynamic>(result.output.ToString());
+                    try
+                    {
+                        if (output.status.ToString().ToLower().Contains("success"))
+                        {
+                            Console.WriteLine(String.Format("{0} {1} success", br.Stack, br.Version));
+                        }
+                        else
+                        {
+                            Console.WriteLine(String.Format("{0} {1} failure", br.Stack, br.Version));
+                            Console.WriteLine(output.error.ToString());
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(String.Format("{0} {1} failure", br.Stack, br.Version));
+                        Console.WriteLine(output.ToString());
+                    }
+                    break;
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(1 * 60 * 1000); // sleep 1 mins between builds
+                }
+            }
         }
     }
 }
